88467306c8fdb1750608e73593f50256a968ad64
kimyoungjae <jae2942@gmail.com>
2023-12-19 09:15:25 +0900

    typescript-study:class

--- src/App.tsx
+++ src/App.tsx
@@ -285,6 +285,25 @@ function 최댓값(...x : number[]) {
 }
 
 
+/*
+(숙제2) x 속성에 숫자를 더해주는 함수가 필요합니다.
+class User {
+  private static x = 10;
+  public static y = 20;
+}
+User.addOne(3) //이렇게 하면 x가 3 더해져야함
+User.addOne(4) //이렇게 하면 x가 4 더해져야함
+User.printX()  //이렇게 하면 콘솔창에 x값이 출력되어야함
+저렇게 User.addOne() 쓸 때마다 x가 증가하는 함수는 어떻게 만들 수 있을까요? 
+
+그리고 x값을 콘솔창에 출력해주는 printX() 함수도 한번 만들어보십시오.
+
+(조건) private static x = 10; 이 코드 수정금지 
+
+*/
+
+
+
   return (
     <div>
 
