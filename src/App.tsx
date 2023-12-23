f5412e43d87b1530c43efc4c025875ec77fcfa10
kim young jae <jae2942@gmail.com>
2023-12-23 22:15:03 +0900

    typescript-study : object index signatures

--- src/App.tsx
+++ src/App.tsx
@@ -438,6 +438,62 @@ unction 함수(...rest :(string|number)[]){
   return result;  
 } 
 */
+
+/**
+(숙제1) 다음 자료의 타입을 지정해보십시오. 
+let obj = {
+  model : 'k5',
+  brand : 'kia',
+  price : 6000,
+  year : 2030,
+  date : '6월',
+  percent : '5%',
+  dealer : '김차장',
+}
+ */
+type ObjType ={
+  [key:string] :string|number
+}
+let obj:ObjType = {
+  model : 'k5',
+  brand : 'kia',
+  price : 6000,
+  year : 2030,
+  date : '6월',
+  percent : '5%',
+  dealer : '김차장',
+}
+/**
+ (숙제2) 다음 object 자료의 타입을 interface 써서 만들어보십시오.
+ let obj = {
+  'font-size' : 10,
+  'secondary' : {
+    'font-size' : 12,
+    'third' : {
+      'font-size' : 14
+    }
+  }
+}
+object 안에 object 안에 object가 들어있습니다.
+타입지정 해보도록 합시다. 물론 비슷한 object들이 
+더 중첩되어도 가능하게 recursive한 타입을 써보는건 어떨까요. 
+ */
+
+interface ObjRecall {
+  [key:string] : ObjRecall |number
+
+}
+
+let obj2:ObjRecall = {
+  'font-size' : 10,
+  'secondary' : {
+    'font-size' : 12,
+    'third' : {
+      'font-size' : 14
+    }
+  }
+}
+console.log(obj2)
 return (
     <div>
 
